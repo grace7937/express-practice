ce5c67fcbb07d2397c7fe4f5189efba4eef5b739
김은총 <eunchong.k153@gmail.com>
2021-05-18 23:18:32 +0900

    Add main-js

---
+++ main.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet')
+app.use(helmet());
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.use(session({
+  secret: 'asadlfkj!@#!@#dfgasdg',
+  resave: false,
+  saveUninitialized: true,
+  store:new FileStore()
+}))
+
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
+
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
+var authRouter = require('./routes/auth');
+
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
+app.use('/auth', authRouter);
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
