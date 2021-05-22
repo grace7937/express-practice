bcab26c5287a9c5ec083ae45ce82c8f29d941fd8
김은총 <eunchong.k153@gmail.com>
2021-05-22 18:39:07 +0900

    Add main-js

--- main.js
+++ main.js
@@ -5,20 +5,13 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const helmet = require('helmet')
 app.use(helmet());
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
 
+const indexRouter = require('./routes/index');
+const topicRouter = require('./routes/topic');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.use(session({
-  secret: 'asadlfkj!@#!@#dfgasdg',
-  resave: false,
-  saveUninitialized: true,
-  store:new FileStore()
-}))
-
 app.get('*', function(request, response, next){
   fs.readdir('./data', function(error, filelist){
     request.list = filelist;
@@ -26,13 +19,8 @@ app.get('*', function(request, response, next){
   });
 });
 
-var indexRouter = require('./routes/index');
-var topicRouter = require('./routes/topic');
-var authRouter = require('./routes/auth');
-
 app.use('/', indexRouter);
 app.use('/topic', topicRouter);
-app.use('/auth', authRouter);
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
