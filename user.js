a5a0bf25027704d89a2f74665ee35416e5437bd4
김은총 <eunchong.k153@gmail.com>
2021-06-04 20:09:12 +0900

    Add add example to user.js

--- user.js
+++ user.js
@@ -19,6 +19,41 @@ const UserSchema = mongoose.Schema({
   silence.save();
 
 
+  ///////////////////////////////////////////////////////////////
+
+
+  // const mongoose = require('mongoose');
+// const { Schema } = mongoose;
+
+// const userSchema = new Schema({
+//     id: String,
+//     pwd: String,
+//   },{timestamps: true});
+
+//   module.exports = mongoose.model('User', userSchema);
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+require("dotenv").config({ path: "variables.env" });
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log("erㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ");
+  } else {
+    console.log("connected to data gㅎㅎ");
+  }
+});
+
+const userSchema = new Schema(
+  {
+    id: String,
+    pwd: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
+
 
 
 
